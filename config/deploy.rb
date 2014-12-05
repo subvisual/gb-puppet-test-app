adf61f696c13f1aee243a0798e1bc0b2af5b9018
Miguel Palhas <mpalhas@gmail.com>
2014-12-05 13:45:52 +0000

    Points deploy to local vagrant virtual machine

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :repo_url, 'git://github.com/groupbuddies/gb-puppet-test-app.git'
 
-server '178.62.191.139', user: 'deploy', roles: %w{web app db}, primary: true
-set :ssh_options, { forward_agent: true }
+server 'localhost', user: 'deploy', roles: %w{web app db}, primary: true
+set :ssh_options, { port: 2222, forward_agent: true }
 
 set :format, :pretty
 set :log_level, :info
@@ -15,7 +15,6 @@ set :linked_dirs,  %w{log public/system}
 set :bundle_without, %w(development test deploy).join(' ')
 set :keep_releases, 3
 
-set :foreman_user_sudo, true
 set :foreman_export_path, '/home/deploy/.init'
 set :foreman_options, {
   user: 'deploy'
